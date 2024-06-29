4da300eb66710f200854f3a5f45770ce6e3c7478
David R Rrrrr <david.rrrrrr@yandex.com>
2024-06-29 14:25:21 -0400

    fixed font name

--- init.el
+++ init.el
@@ -279,7 +279,7 @@
   ;; <https://github.com/protesilaos/iosevka-comfy>.
   (setq fontaine-presets
         '((small
-           :default-family "IosevkaTerm Nerd Font"
+           :default-family "FiraMono Nerd Font"
            :default-height 120
            :variable-pitch-family "Liberation Sans")
           (regular) ; like this it uses all the fallback values and is named `regular'
@@ -296,7 +296,7 @@
            ;; I keep all properties for didactic purposes, but most can be
            ;; omitted.  See the fontaine manual for the technicalities:
            ;; <https://protesilaos.com/emacs/fontaine>.
-           :default-family "IosevkaTerm Nerd Font"
+           :default-family "FiraMono Nerd Font"
            :default-weight regular
            :default-height 160
 
@@ -516,7 +516,7 @@
                 (if (equal window-system 'w32)
                     (async-shell-command "explorer .")
                   (if (equal window-system 'x)
-                      (async-shell-command "thunar ."))))))
+                      (async-shell-command "dolphin ."))))))
 
 (require 'flyspell)
 (add-hook 'text-mode-hook 'flyspell-mode)
@@ -557,7 +557,74 @@
   )
 
 (when (equal window-system 'x)
-  (use-package notmuch))
+  ;; place linux programs here
+  (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+  (require 'mu4e)
+  ;; use mu4e for e-mail in emacs
+  (setq mail-user-agent 'mu4e-user-agent)
+
+  (setq mu4e-drafts-folder "/gmail/[Gmail]/Drafts")
+  (setq mu4e-sent-folder   "/gmail/[Gmail]/Sent Mail")
+  (setq mu4e-trash-folder  "/gmail/[Gmail]/Trash")
+  (setq mu4e-refile-folder "/gmail/[Gmail]/All Mail")
+
+  ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+  (setq mu4e-sent-messages-behavior 'delete)
+
+  ;; (See the documentation for `mu4e-sent-messages-behavior' if you have
+  ;; additional non-Gmail addresses and want assign them different
+  ;; behavior.)
+
+  ;; setup some handy shortcuts
+  ;; you can quickly switch to your Inbox -- press ``ji''
+  ;; then, when you want archive some messages, move them to
+  ;; the 'All Mail' folder by pressing ``ma''.
+
+  (setq mu4e-maildir-shortcuts
+	'( (:maildir "/gmail/INBOX"              :key ?i)
+	   (:maildir "/gmail/[Gmail]/Sent Mail"  :key ?s)
+	   (:maildir "/gmail/[Gmail]/Trash"      :key ?t)
+	   (:maildir "/gmail/[Gmail]/All Mail"   :key ?a)))
+
+  (add-to-list 'mu4e-bookmarks
+	       ;; ':favorite t' i.e, use this one for the modeline
+	       '(:query "maildir:/inbox" :name "Inbox" :key ?i :favorite t))
+
+  ;; allow for updating mail using 'U' in the main view:
+  ;; (setq mu4e-get-mail-command "")
+
+  ;; something about ourselves
+  (setq
+   user-mail-address "daverecor@gmail.com"
+   user-full-name  "David Recor"
+   message-signature
+   (concat
+    "David Recor"))
+
+  ;; sending mail -- replace USERNAME with your gmail username
+  ;; also, make sure the gnutls command line utils are installed
+  ;; package 'gnutls-bin' in Debian/Ubuntu
+
+  (require 'smtpmail)
+  (setq message-send-mail-function 'smtpmail-send-it
+	starttls-use-gnutls t
+	smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+	smtpmail-auth-credentials
+	'(("smtp.gmail.com" 587 "daverecor@gmail.com" nil))
+	smtpmail-default-smtp-server "smtp.gmail.com"
+	smtpmail-smtp-server "smtp.gmail.com"
+	smtpmail-smtp-service 587)
+
+  ;; alternatively, for emacs-24 you can use:
+  ;;(setq message-send-mail-function 'smtpmail-send-it
+  ;;     smtpmail-stream-type 'starttls
+  ;;     smtpmail-default-smtp-server "smtp.gmail.com"
+  ;;     smtpmail-smtp-server "smtp.gmail.com"
+  ;;     smtpmail-smtp-service 587)
+
+  ;; don't keep message buffers around
+  (setq message-kill-buffer-on-exit t)
+  )
 
 (use-package casual-info
   :ensure t
@@ -679,3 +746,25 @@
   (kill-ring-save (point-min) (point-max)))
 
 (global-set-key (kbd "C-c v") 'drr-copy-all-text)
+
+(defun drr-add-double-quotes-and-commas ()
+  "Add double quotes before and after each line with a comma at the end."
+  (interactive)
+  (goto-char (point-min))
+  (while (not (eobp))
+    (beginning-of-line)
+    (insert "\"")
+    (end-of-line)
+    (insert "\",")
+    (forward-line 1)))
+
+(defun drr-add-single-quotes-and-commas ()
+  "Add single quotes before and after each line with a comma at the end."
+  (interactive)
+  (goto-char (point-min))
+  (while (not (eobp))
+    (beginning-of-line)
+    (insert "\'")
+    (end-of-line)
+    (insert "\',")
+    (forward-line 1)))
